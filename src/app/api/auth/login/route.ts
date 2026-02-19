a042bab0a3f0f487f5bbc4c23a5b81e2eb45487d
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2026-02-19 22:04:00 +0530

    implement GET route for authentication login with redirect to authorization URL

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+import { scalekit } from "@/lib/scalekit";
+
+export async  function GET(req:NextRequest) {
+    const redirectUri =`${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`
+  const url=  scalekit.getAuthorizationUrl(redirectUri)
+    console.log(url);
+    
+return NextResponse.redirect(url)   
+}
